e33de2c8854423e8aa1255fd0ef315485d2f009c
Shota Narisaka <shootacean@gmail.com>
2024-04-08 21:59:58 +0000

    add Text Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Button, Center, Grid, GridItem } from "@chakra-ui/react";
+import { Text, Textarea, Center, Grid, GridItem } from "@chakra-ui/react";
 import { useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [input, setInput] = useState<string>("");
 
 	return (
 		<>
 			<Grid
 				templateAreas={`"header header"
-                        "nav main"
+                        "main main"
                         "footer footer"`}
 				gridTemplateRows={"50px auto 50px"}
 				gridTemplateColumns={"150px 1fr"}
@@ -17,22 +17,17 @@ function App() {
 				color="blackAlpha.700"
 				fontWeight="bold"
 			>
+				{/* TODO : Header */}
 				<GridItem pl="2" bg="orange.300" area={"header"}>
 					Header
 				</GridItem>
-				<GridItem pl="2" bg="pink.300" area={"nav"}>
-					Nav
-				</GridItem>
-				<GridItem pl="2" bg="green.300" area={"main"}>
+				<GridItem p="8" area={"main"}>
 					<Center height="full">
-						<Button
-							colorScheme="blue"
-							onClick={() => setCount((count) => count + 1)}
-						>
-							count is {count}
-						</Button>
+						<Textarea value={input} onChange={(e) => setInput(e.target.value)}/>
 					</Center>
+					<Text>count: {input.length}</Text>
 				</GridItem>
+				{/* TODO : Footer */}
 				<GridItem pl="2" bg="blue.300" area={"footer"}>
 					Footer
 				</GridItem>
